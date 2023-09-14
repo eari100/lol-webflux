ef96a36c582fba86ae4a6fe60a6dc163b1ff2582
Jaewook Choi <eari100@naver.com>
2023-09-15 04:41:49 +0900

    Initial commit

---
+++ src/main/java/com/example/lolwebflux/LolWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.example.lolwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolWebfluxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lolwebflux/LolWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lolwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
